5c205203bbc7340e1db74dc052c8e2d0c3ac04b6
Mimi Quinsayas - A17909 <indaleen.quinsayas@microchip.com>
2021-04-21 17:42:16 +0800

    initial commit of class demos

---
+++ ArrayDemo/ArrayDemoProgram.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArrayDemo
+{
+    class ArrayDemoProgram
+    {
+        static void Main(string[] args)
+        {
+
+            var booktitles = new List<string>() { "Brain Rules", "PragmaticProgrammer", 
+                "Harry Potter and the Half-Blood Prince", "The Alchemist" };
+
+            //retrieving an element from an array
+            Console.WriteLine(booktitles[0]);
+            Console.WriteLine(booktitles[1]);
+            Console.WriteLine(booktitles[2]);
+            Console.WriteLine(booktitles[3]);
+
+
+            //var books = new List<string>() { "HPs", "The Alchemist" };
+            var books = new List<string>();
+            ArrayDemoProgram first = new ArrayDemoProgram();
+
+            first.DisplayElements(books);
+            first.Test("hello");
+        }
+
+        public void DisplayElements(List<string> collections)
+        {
+            if (collections.Count > 0) {
+                foreach (var item in collections)
+                {
+                    Console.WriteLine("Item is " + item);
+                }
+            }
+        }
+
+        public void Test(string y, int x = 2)
+        {
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+    }
+}
---
+++ ArrayListMethodDemo/ArrayListMethodDemoProgram.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArrayListMethodDemo
+{
+    class ArrayListMethodDemoProgram
+    {
+        static void Main(string[] args)
+        {
+            var booktitles = new[] {"Brain Rules",
+                            "Pragmatic Programmer",
+                            "Harry Potter and the Half-Blood Prince",
+                            "The Alchemist",
+                            "Deep Work",
+                            "The Alchemist"};
+
+            var indexOfAlchemist = Array.IndexOf(booktitles, "The Alchemist");
+            booktitles.SetValue("The Spy", 5);
+
+            for (int i = 0; i < booktitles.Length; i++)
+            {
+                booktitles[i] = booktitles[i].ToUpper();
+            }
+
+            foreach (var title in booktitles)
+            {
+                Console.WriteLine($"Title is {title}");
+            }
+
+            var books = new List<string>()
+                {"Brain Rules","Pragmatic Programmer",
+                 "Harry Potter and the Half-Blood Prince",
+                 "The Alchemist","Deep Work"};
+            books.Add("HP 2");
+            var newBooks = new List<string>() { "Book 1", "Book 2" };
+            books.AddRange(newBooks);
+            books.Insert(3, "HP 3");
+            books.Remove("HP 2");
+            books.Remove("HP 3");
+
+            foreach (var title in books)
+            {
+                Console.WriteLine($"Title is {title}");
+            }
+
+            List<string> authors = new List<string>();
+            DisplayElements(authors);
+        }
+
+        static void DisplayElements(List<string> collections)
+        {
+            Display(collections);
+        }
+        static void DisplayElements(string[] stringArray)
+        {
+            Display(stringArray);
+        }
+
+        static void Display(IEnumerable<string> collection)
+        {
+            foreach (var item in collection)
+            {
+                Console.WriteLine("Item is " + item);
+                Console.WriteLine();
+            }
+        }
+
+       
+
+    }
+}
